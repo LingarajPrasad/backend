12a3c94f7c69805faba2da0cbce216614e2b5ac0
Lingaraj Prasad Sahu <lingarajprasadsahu2002@gmail.com>
2024-03-28 21:24:55 +0530

    suggestions added

--- controllers/userController.js
+++ controllers/userController.js
@@ -193,4 +193,32 @@ const updateUser = async (req, res) => {
         console.log("Error in updateUser: ", err.message)
     }
 }
-export { signupUser, loginUser, updateUser, logoutUser, followUnfollowUser, getUserProfile }
+
+const getSuggestedUsers = async (req, res) => {
+	try {
+		// exclude the current user from suggested users array and exclude users that current user is already following
+		const userId = req.user._id;
+
+		const usersFollowedByYou = await User.findById(userId).select("following");
+
+		const users = await User.aggregate([
+			{
+				$match: {
+					_id: { $ne: userId },
+				},
+			},
+			{
+				$sample: { size: 10 },
+			},
+		]);
+		const filteredUsers = users.filter((user) => !usersFollowedByYou.following.includes(user._id));
+		const suggestedUsers = filteredUsers.slice(0,100);
+
+		suggestedUsers.forEach((user) => (user.password = null));
+
+		res.status(200).json(suggestedUsers);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+export { signupUser, loginUser, updateUser, logoutUser, followUnfollowUser, getUserProfile ,getSuggestedUsers}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {getUserProfile,signupUser,updateUser,loginUser,logoutUser,followUnfollowUser} from '../controllers/userController.js'
+import {getUserProfile,signupUser,updateUser,loginUser,logoutUser,followUnfollowUser,getSuggestedUsers} from '../controllers/userController.js'
 import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router()
 
 
 router.get('/profile/:query',getUserProfile)
+router.get('/suggested',protectRoute,getSuggestedUsers)
 router.post('/signup',signupUser)
 router.post('/login',loginUser)
 router.post('/logout',logoutUser)
